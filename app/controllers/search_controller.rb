c2772033789bb1adfea5d9a7cb108be55985a72f
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 11:06:32 -0600

    remove logic from controller to LocationSearchService

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    conn = Faraday.new("https://developer.nrel.gov")
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params[:q]}&radius=6.0&fuel_type=ELEC,LPG&limit=10")
-    results = JSON.parse(response.body)
-    @stations = results["fuel_stations"].map { |station| Station.new(station)}
+    service = LocationSearchService.new(params[:q], "6.0", "10")
+    @stations = service.stations
   end
 
 end
