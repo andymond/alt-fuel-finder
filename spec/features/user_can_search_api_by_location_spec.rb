512726cdd9444a8897e4fb6a3e260cac2ca19ddf
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 09:28:34 -0600

    Add search controller w/ index action & view"

--- spec/features/user_can_search_api_by_location_spec.rb
+++ spec/features/user_can_search_api_by_location_spec.rb
@@ -11,8 +11,10 @@ require "rails_helper"
 describe "user visits root" do
   it "can search api by zipcode" do
     visit root_path
-    fill_in "input", with: "80203"
-    click_on "Locate"
+    within(".navbar") do
+      fill_in "q", with: "80203"
+      click_on "Locate"
+    end
 
     expect(current_path).to eq("/search?zipcode=80203")
     expect(page).to have_content(search_results)
