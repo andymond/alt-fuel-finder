5cb49dad03bec82e7a927be90b6673455600924a
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 10:38:05 -0600

    Format search results view & call station poro in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     conn = Faraday.new("https://developer.nrel.gov")
     response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params[:q]}&radius=6.0&fuel_type=ELEC,LPG&limit=10")
     results = JSON.parse(response.body)
-    @stations = results["fuel_stations"]
+    @stations = results["fuel_stations"].map { |station| Station.new(station)}
   end
 
 end
