2305bbe3d1d2194bbb5925def5ab945295a76403
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 11:00:07 -0600

    change results method name to stations

--- spec/services/location_search_service_spec.rb
+++ spec/services/location_search_service_spec.rb
@@ -4,9 +4,9 @@ describe LocationSearchService do
   zip, distance, limit = "80203", "6.0", "10"
   let (:service) { LocationSearchService.new(zip, distance, limit) }
   it "#instance methods" do
-    expect(service.results).to be_an Array
-    expect(service.results.first).to be_a Station
-    expect(service.results.count).to eq(10)
-    expect(service.results.first.name).to eq("UDR")
+    expect(service.stations).to be_an Array
+    expect(service.stations.first).to be_a Station
+    expect(service.stations.count).to eq(10)
+    expect(service.stations.first.name).to eq("UDR")
   end
 end
