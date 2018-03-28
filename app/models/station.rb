25008258796d21648ec9dc396811154e049033aa
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 10:37:23 -0600

    reformat address method in Station

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,13 +1,21 @@
 class Station
 
-  attr_reader :name, :address, :fuel_types, :distance, :access_times
+  attr_reader :name, :fuel_types, :distance, :access_times
 
   def initialize(attributes)
-    @name = attributes["name"]
-    @address = attributes["address"]
+    @name = attributes["station_name"]
+    @street_address = attributes["street_address"]
+    @city = attributes["city"]
+    @state = attributes["state"]
+    @zipcode = attributes["zip"]
+    @country = attributes["country"]
     @fuel_types = attributes["fuel_type_code"]
     @distance = attributes["distance"]
     @access_times = attributes["access_days_time"]
+  end
+
+  def address
+    [@street_address, "\n", @city, ", ", @state, "\n", @zipcode, ", ", @country].join
   end
 
 end
