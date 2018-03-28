205a407fb9d6251d5ca60d5d294c141c87c9c6c0
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 10:28:01 -0600

    Modify station_spec to expect formatted address

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,11 +3,23 @@ require "rails_helper"
 # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
 
 describe Station do
-  let(:attrs)  { { "name" => "test_name", "address" => "test_address", "fuel_type_code" => "test_fuel", "distance" => "1", "access_days_time" => "247" } }
+  let(:attrs)  {
+                  { "station_name" => "test_name",
+                   "city" => "city",
+                   "street_address" => "street_address",
+                   "state" => "state",
+                   "zip" => "zipcode",
+                   "country" => "country",
+                   "fuel_type_code" => "test_fuel",
+                   "distance" => "1",
+                   "access_days_time" => "247"
+                  }
+                }
   let(:station) { Station.new(attrs) }
+
   it "responds to attributes" do
     expect(station.name).to eq("test_name")
-    expect(station.address).to eq("test_address")
+    expect(station.address).to eq("street_address\ncity, state\nzipcode, country")
     expect(station.fuel_types).to eq("test_fuel")
     expect(station.distance).to eq("1")
     expect(station.access_times).to eq("247")
