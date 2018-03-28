dddb2c07175437020b143ffd0a6e87311ca494f1
Andrew Dymond <andy.dymond@gmail.com>
2018-03-28 09:38:20 -0600

    Add api connection in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-
+    conn = Faraday.new("https://developer.nrel.gov")
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&location=#{params[:q]}")
   end
-  
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,20 +5,20 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'webmock/rspec'
-require 'vcr'
+# require 'webmock/rspec'
+# require 'vcr'
 
-def stub_get(filename, url)
-  json_response = File.read("./spec/fixtures/#{filename}")
-  stub_request(:get, url)
-    .to_return(status: 200, body: "#{json_response}", headers: {})
-end
+# def stub_get(filename, url)
+#   json_response = File.read("./spec/fixtures/#{filename}")
+#   stub_request(:get, url)
+#     .to_return(status: 200, body: "#{json_response}", headers: {})
+# end
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/cassettes"
-  config.hook_into :webmock
-  config.filter_sensitive_data('<NREL_KEY>') { ENV["NREL_API_KEY"]}
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/cassettes"
+#   config.hook_into :webmock
+#   config.filter_sensitive_data('<NREL_KEY>') { ENV["NREL_API_KEY"]}
+# end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
